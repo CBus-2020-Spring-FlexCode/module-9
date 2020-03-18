e7310d611b237aedf066d235acfc69614cbea97b
Zach012 <zachdillon012@gmail.com>
2020-03-17 21:26:14 -0400

    added mvc test

--- src/test/java/org/wcci/reviews/ReviewControllerMockMvcTest.java
+++ src/test/java/org/wcci/reviews/ReviewControllerMockMvcTest.java
@@ -3,12 +3,15 @@ package org.wcci.reviews;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import javax.annotation.Resource;
 
+import org.assertj.core.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -27,8 +30,14 @@ public class ReviewControllerMockMvcTest {
 	@MockBean
 	private ReviewRepository reviewRepo;
 	
+	@MockBean
+	private CategoryRepository categoryRepo;
+	
 	@Mock
 	private Review review;
+	
+	@Mock
+	private Review anotherReview;
 	
 	@Test
 	public void shouldRouteToSingleReview() throws Exception {
@@ -36,5 +45,39 @@ public class ReviewControllerMockMvcTest {
 		when(reviewRepo.findById(reviewId)).thenReturn(Optional.of(review));
 		mvc.perform(get("/review?id=1")).andExpect(view().name(is("review")));
 	}
+	
+	@Test
+	public void shouldBeOkayForSingleReview()throws Exception {
+		long reviewId = 1;
+		when(reviewRepo.findById(reviewId)).thenReturn(Optional.of(review));
+		mvc.perform(get("/show-review?id=1")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldPutSingleReviewIntoModel() throws Exception {
+		when(reviewRepo.findById(1L)).thenReturn(Optional.of(review));
+		
+		mvc.perform(get("/review?id=1")).andExpect(model().addAttribute("reviews", (is(review))));
+	}
+	
+	@Test
+	public void shouldRouteToAllReviews() throws Exception {
+		mvc.perform(get("/show-reviews")).andExpect(view().name(is("reviews")));
+	}
+	
+	@Test
+	public void shouldBeOkayForAllReviews() throws Exception {
+		mvc.perform(get("/show-reviews")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldPutAllReviewsIntoModel() throws Exception {
+		Collection<Review> allReviews = Arrays.asList(review, anotherReview);
+		when(reviewRepo.findAll()).thenReturn(allReviews);
+		
+		mvc.perform(get("/show-reviews")).andExpect(model().attribute("reviews", is(allReviews)));
+	}
+	
+	
 
 }
