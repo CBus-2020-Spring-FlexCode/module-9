a0b93647380c97a11cc7b4f16aec208db1116e89
Evan Novak <evannvk@gmail.com>
2020-03-17 20:35:10 -0400

    Fixed review and category classes

--- src/main/java/org/wcci/reviews/Category.java
+++ src/main/java/org/wcci/reviews/Category.java
@@ -1,10 +1,6 @@
 package org.wcci.reviews;
 
-<<<<<<< HEAD
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,8 +14,8 @@ public class Category {
 	private long id;
 	private String name;
 	
-    //@OneToMany(mappedBy = "categories")
-	//private Collection<Review> reviews; 
+    @OneToMany(mappedBy = "categories")
+	private Collection<Review> reviews; 
 
 	public long getId() {
 		return id;
@@ -37,64 +33,15 @@ public class Category {
 		this.name = name;
 	}
 
-//	public Collection<Review> getReviews() {		
-//		return reviews;
-//	}
-=======
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
-
-@Entity
-public class Category {
-	@Id
-	@GeneratedValue
-	private Long id;
-
-	@ManyToOne
-	private Review review;
-	private String name;
-
-	public Category() {
-
+	public Collection<Review> getReviews() {		
+		return reviews;
 	}
-
-	public Category(String name, Review review) {
-		this.name = name;
-		this.review = review;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public Review getReview() {
-		return review;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public String toString() {
-		return "Category [id=" + id + ", review=" + review + ", name=" + name + "]";
-	}
->>>>>>> origin/module-9-dillon
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-<<<<<<< HEAD
 		result = prime * result + (int) (id ^ (id >>> 32));
-=======
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((review == null) ? 0 : review.hashCode());
->>>>>>> origin/module-9-dillon
 		return result;
 	}
 
@@ -107,33 +54,9 @@ public class Category {
 		if (getClass() != obj.getClass())
 			return false;
 		Category other = (Category) obj;
-<<<<<<< HEAD
 		if (id != other.id)
-			return false;
-		return true; 
-	}
-
-	
-	
-}
-=======
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (review == null) {
-			if (other.review != null)
-				return false;
-		} else if (!review.equals(other.review))
 			return false;
 		return true;
 	}
-
+	
 }
->>>>>>> origin/module-9-dillon
--- src/main/java/org/wcci/reviews/Review.java
+++ src/main/java/org/wcci/reviews/Review.java
@@ -1,35 +1,28 @@
 package org.wcci.reviews;
 
-<<<<<<< HEAD
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
-=======
 import java.util.Collection;
->>>>>>> origin/module-9-dillon
+
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-<<<<<<< HEAD
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
-=======
 import javax.persistence.OneToMany;
->>>>>>> origin/module-9-dillon
+
 
 @Entity
 public class Review {
 	
-<<<<<<< HEAD
-=======
-	private String name;
->>>>>>> origin/module-9-dillon
+
 	@Id
 	@GeneratedValue
 	private Long id;
 	
-<<<<<<< HEAD
+
 	private String title;
 	private String image;
 	@Lob
@@ -37,10 +30,6 @@ public class Review {
 	
 	@ManyToMany
 	private Collection<Category> categories;
-	
-	public long getId() {
-		return id;
-	}
 	
 	//Possibly name, depends on how others name it
 	public String getTitle() {
@@ -69,26 +58,10 @@ public class Review {
 		this.image = image;
 		this.content = content;
 		this.categories = new HashSet<>(Arrays.asList(categories));
-=======
-	@OneToMany(mappedBy="review")
-	private Collection<Category> categories;
+	}
 	
-	public Review() {
-		
-	}
-
-	public Review(String name, String description, String image, Category category) {
-		this.name = name;
-	}
-
 	public Long getId() {
 		return id;
-	}
-
-	@Override
-	public String toString() {
-		return "Review [name=" + name + ", id=" + id + "]";
->>>>>>> origin/module-9-dillon
 	}
 
 	@Override
@@ -96,10 +69,6 @@ public class Review {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-<<<<<<< HEAD
-=======
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
->>>>>>> origin/module-9-dillon
 		return result;
 	}
 
@@ -117,23 +86,7 @@ public class Review {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-<<<<<<< HEAD
 		return true;
 	}
-	
-=======
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
-
-	public Iterable<Category> getCategories() {
-		
-		return categories;
-	}
->>>>>>> origin/module-9-dillon
 
 }
